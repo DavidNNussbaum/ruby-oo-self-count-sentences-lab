dee6bf5bd9fa96de222cec5db848d0e623ec7f71
DavidNNussbaum <brocknynj@aol.com>
2020-11-08 15:30:02 -0500

    commit tree ee15e278b557b4e21661017eb016141a9a0d1c0d

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    # amt = self.split("." || "?" || "!")
+      #  amt = self.split( /[.?!]/)
+   
+       amt = self.split(/\.+|\!+|\?+/)
+      #  binding.pry
+       tot = amt.count
+      
+      #  amt.each do |item|
+      #  amt.count {|item| item} 
+      # binding.pry
+      # tot = item.count
+       
+       tot
   end
+
 end
